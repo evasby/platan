8652a06a2ab1f83cf1283291183d88da8526be3d
Vladimir Dmitriev <defurator@mail.ru>
2016-06-09 17:24:38 +0200

    personal -

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-
   $(".slider_list").lightSlider({
     item: 1,
     controls: false,
@@ -18,60 +17,36 @@ $(document).ready(function(){
   $(".partners_right").click(function(){
     partners.goToNextSlide()
   });
-  /***********************************/
-  /***********************************/
-  if($("#map").length>0) {
-    var map;
-    var myLatlng = new google.maps.LatLng(53.8749103, 27.6714312);
-    var myCenter = new google.maps.LatLng(53.8749103, 27.6714312);
-    function initialize() {
-      /*var styles = [
-        {
-          stylers: [
-            { hue: "#cccccc" },
-            { saturation: -120 }
-          ]
-        },{
-          featureType: "road",
-          elementType: "geometry",
-          stylers: [
-            { lightness: 100 },
-            { visibility: "simplified" }
-          ]
-        },{
-          featureType: "road",
-          elementType: "labels",
-          stylers: [
-            { visibility: "off" }
-          ]
-        }
-      ];*/
-      /*var styledMap = new google.maps.StyledMapType(styles,
-      {name: "Styled Map"});*/
-      var mapOptions = {
-        zoom: 17,
-        center: myCenter
-        /*mapTypeControlOptions: {
-          mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'map_style']
-        }*/
-      };
-      map = new google.maps.Map(document.getElementById('map'),
-          mapOptions);
-      var marker = new google.maps.Marker({
-          position: myLatlng,
-          title:"Webformat"
-          //icon: '../images/footer-marker.png'
-      });
-      marker.setMap(map);
-      var contentString = '<section class="location"><h1>ООО “КАМФРИ”</h1><p>+375 (29) <b>574 93 20</b>; +375 (29) <b>574 93 20</b></p><p>г. Минск, ул. Байкальская, 58/1</p></section>';
-      var infowindow = new google.maps.InfoWindow({
-          content: contentString
-      });
-      infowindow.open(map,marker);
-      map.mapTypes.set('map_style', styledMap);
-      map.setMapTypeId('map_style');
+  $(".entry_link").colorbox({inline:true, href:"#form-entire", opacity: 0.8, top: "20%"});
+  $(".basket-table_photo").colorbox({className: 'ctable', transition: 'none'});
+  $(".basket_link").click(function(){
+    $(".basket-popup").show();
+  });
+  $(document).mouseup(function (e) {
+    var container = $(".basket-popup");
+    if (container.has(e.target).length === 0){
+        container.hide();
     }
-    google.maps.event.addDomListener(window, 'load', initialize);
-    /************************************************************************/
+  });
+  /***********************************/
+  if($("#yamap").length>0) {
+    ymaps.ready(init);
   }
+  var myMap;
+  function init(){     
+    myMap = new ymaps.Map("yamap", {
+        center: [59.90648525806242,30.372057499999976],
+        zoom: 15
+    });
+    myPlacemark = new ymaps.Placemark([59.90648525806242,30.372057499999976], {   
+      },{
+        iconLayout: 'default#image',
+        iconImageHref: '../images/yamap-marker.png',
+        iconImageSize: [59, 62],
+        iconImageOffset: [-26, -62]
+    });
+
+    myMap.geoObjects.add(myPlacemark);
+  }
+  /***********************************/
 });
